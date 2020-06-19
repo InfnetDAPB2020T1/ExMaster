bfa405ca3e81fb3ef33850d255a7b78174c825ab
Thiago Vieira de Aguiar <thiago.vieiradeaguiar@gmail.com>
2020-06-19 11:53:16 -0300

    ListFragment: Recycler Setup

--- app/src/main/java/com/example/exmaster/ui/list/ListFragment.kt
+++ app/src/main/java/com/example/exmaster/ui/list/ListFragment.kt
@@ -9,6 +9,7 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import com.example.exmaster.R
+import kotlinx.android.synthetic.main.fragment_home.*
 
 class ListFragment : Fragment() {
 
@@ -22,10 +23,14 @@ class ListFragment : Fragment() {
         listViewModel =
                 ViewModelProviders.of(this).get(ListViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_home, container, false)
-        val textView: TextView = root.findViewById(R.id.text_home)
-        listViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
-        })
         return root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        listViewModel.setupRecycler(
+            rcyVwCards, this@ListFragment.requireContext(),
+            pgrBarListCards
+        )
     }
 }
--- app/src/main/java/com/example/exmaster/ui/list/ListViewModel.kt
+++ app/src/main/java/com/example/exmaster/ui/list/ListViewModel.kt
@@ -1,6 +1,8 @@
 package com.example.exmaster.ui.list
 
 import android.content.Context
+import android.view.View
+import android.widget.ProgressBar
 import android.widget.Toast
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
@@ -9,6 +11,7 @@ import androidx.recyclerview.widget.RecyclerView
 import com.example.exmaster.adapter.CardsAdapter
 import com.example.exmaster.apiclient.ApiClient
 import com.example.exmaster.apiclient.model.Deck
+import kotlinx.android.synthetic.main.fragment_home.*
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -17,17 +20,19 @@ class ListViewModel : ViewModel() {
 
     fun setupRecycler(
         recyclerView: RecyclerView,
-        context: Context
+        context: Context,
+        pgrBarListCards: ProgressBar
     ){
         // Chamar a API
+        pgrBarListCards.visibility = View.VISIBLE
         val call = ApiClient.getDeckService().all()
         call.enqueue(object : Callback<Deck> {
             override fun onFailure(call: Call<Deck>, t: Throwable) {
+                pgrBarListCards.visibility = View.GONE
                 Toast.makeText(
                     context, t.message, Toast.LENGTH_LONG
                 ).show()
             }
-
             override fun onResponse(call: Call<Deck>, response: Response<Deck>) {
                 val deck = response.body()
                 if (deck != null && !deck.cards.isNullOrEmpty()){
@@ -37,10 +42,8 @@ class ListViewModel : ViewModel() {
                         context, "Não foi encontrado nenhum card.", Toast.LENGTH_LONG
                     ).show()
                 }
+                pgrBarListCards.visibility = View.GONE
             }
-
         })
-        // se tem retorno montar a Recycler
-        // se não, notificar o usuario
     }
 }
