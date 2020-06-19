6650c7d5938ef7de8f34ebb5a1e96ba9a6360e58
Thiago Vieira de Aguiar <thiago.vieiradeaguiar@gmail.com>
2020-06-19 11:41:39 -0300

    CardsAdapter; card_recycler_item

--- app/src/main/java/com/example/exmaster/adapter/CardsAdapter.kt
+++ app/src/main/java/com/example/exmaster/adapter/CardsAdapter.kt
@@ -1,4 +1,40 @@
 package com.example.exmaster.adapter
 
-class CardsAdapter {
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.exmaster.R
+import com.example.exmaster.apiclient.model.Card
+
+class CardsAdapter (
+    var cards: MutableList<Card>
+) : RecyclerView.Adapter<CardsAdapter.CardsViewHolder>()
+{
+    class CardsViewHolder(itemView: View) // layout XML
+        : RecyclerView.ViewHolder(itemView){
+        val txtVwName: TextView = itemView.txtVwName
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int)
+            : CardsViewHolder {
+        val view = LayoutInflater
+            .from(parent.context)
+            .inflate(
+                R.layout.card_recycler_item,
+                parent,
+                false
+            )
+        val cardsViewHolder = CardsViewHolder(view)
+        return cardsViewHolder
+    }
+
+    override fun getItemCount(): Int = cards.size
+
+    override fun onBindViewHolder(holder: CardsViewHolder, position: Int) {
+        val card = cards[position]
+        holder.txtVwName.text = card.name
+
+    }
 }
