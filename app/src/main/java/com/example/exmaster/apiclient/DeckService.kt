ab4ce3f1b177e181b78c2f1db0fe005b72e04a75
Thiago Vieira de Aguiar <thiago.vieiradeaguiar@gmail.com>
2020-06-23 13:57:02 -0300

    ListFragment: Recycler Setup

--- app/src/main/java/com/example/exmaster/apiclient/DeckService.kt
+++ app/src/main/java/com/example/exmaster/apiclient/DeckService.kt
@@ -6,11 +6,14 @@ import com.example.exmaster.apiclient.model.Deck
 import retrofit2.Call
 import retrofit2.http.GET
 import retrofit2.http.Path
+import retrofit2.http.Query
 
 interface DeckService {
 
     @GET("v1/cards")
-    fun all(): Call<Deck> // {}
+    fun all(
+        @Query("page") page: Int = 1,
+        @Query("pageSize") pageSize: Int = 10): Call<Deck> // {}
 
     @GET("v1/cards/{id}")
     fun show(@Path("id") id: String): Call<CardResponse>
